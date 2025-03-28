5878d470f0fe3624400bbf684f71f291d060dc4b
KwaHash <cosmosray001@gmail.com>
2025-03-28 11:21:39 +0900

    feat: wrap react query provider in layout

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,4 +1,5 @@
 import { type PropsWithChildren } from 'react'
+import ReactQueryProvider from '@/providers/react-query-provider'
 import ReduxProvider from '@/providers/redux-provider'
 import { ThemeProvider } from '@/providers/theme-provider'
 
@@ -10,7 +11,11 @@ export default function Providers({ children }: PropsWithChildren) {
       enableSystem
       disableTransitionOnChange
     >
-      <ReduxProvider>{children}</ReduxProvider>
+      <ReactQueryProvider>
+        <ReduxProvider>
+          {children}
+        </ReduxProvider>
+      </ReactQueryProvider>
     </ThemeProvider>
   )
 }
--- providers/react-query-provider.tsx
+++ providers/react-query-provider.tsx
@@ -6,7 +6,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { getQueryClient } from '@/app/get-query-client'
 
-export function ReactQueryProvider({ children }: PropsWithChildren) {
+export default function ReactQueryProvider({ children }: PropsWithChildren) {
   const queryClient = getQueryClient()
 
   return (
