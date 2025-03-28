d005c1f6bc73d72914c017202cbafc650794ca0a
KwaHash <cosmosray001@gmail.com>
2025-03-28 11:17:09 +0900

    feat: react query provider

---
+++ providers/react-query-provider.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+// Since QueryClientProvider relies on useContext under the hood, we have to put 'use client' on top
+import { type PropsWithChildren } from 'react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { getQueryClient } from '@/app/get-query-client'
+
+export function ReactQueryProvider({ children }: PropsWithChildren) {
+  const queryClient = getQueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
